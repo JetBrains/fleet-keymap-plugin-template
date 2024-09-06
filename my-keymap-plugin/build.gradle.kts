953be9035a2346e85b716a60768950088e87fbbc
Titouan BION <titouan.bion@jetbrains.com>
2024-09-06 10:34:53 +0200

    Update build.gradle.kts

--- my-keymap-plugin/build.gradle.kts
+++ my-keymap-plugin/build.gradle.kts
@@ -9,22 +9,15 @@ fleetPlugin {
 
     // STEP 1: chose an ID for your plugin, it must be unique
     //
-    id = "titouan.bion.test.plugin7"
+    // id = "your.plugin.id.here"
 
     // STEP 2: set up the sensible metadata for your plugin
-    metadata {
-        readableName = "Some test of Titouan 7"
-        description = "Your plugin description here"
-    }
+    // metadata {
+    //     readableName = "Your plugin name here"
+    //     description = "Your plugin description here"
+    // }
 
     fleetRuntime {
         version = libs.versions.fleet.runtime
-    }
-
-    // STEP 3: add publishing information
-    publishing {
-//      vendorId = "773b5309-867b-4724-92c8-0760db075bc0"
-    //     licenseUrl = "your-license-url-here"
-    //     sourceCodeUrl = "your-source-repository-url-here"
     }
 }
